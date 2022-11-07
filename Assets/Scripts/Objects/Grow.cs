b27cb0781c18bb07d42c0b888da6b61c6ae25a7f
Yonatan Golan <yongolan93@gmail.com>
2022-11-07 04:08:20 +0200

    Growing trees can be destroyed now

--- Assets/Scripts/Objects/Grow.cs
+++ Assets/Scripts/Objects/Grow.cs
@@ -6,7 +6,10 @@ public class Grow : MonoBehaviour
 {
     public GameObject GivingTree;
     float Clock;
+    public bool AtRange;
     Vector3 scaleChange;
+    [SerializeField] Transform Log1;
+    [SerializeField] Transform Log2;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +20,44 @@ public class Grow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.localScale += scaleChange;
+        if (AtRange == true)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Instantiate(Log1, transform.position, Quaternion.identity);
+                Instantiate(Log2, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+
+    gameObject.transform.localScale += scaleChange;
         Clock -= Time.deltaTime;
         if (Clock <= 0)
         {
             Instantiate(GivingTree, transform.position, Quaternion.identity);
             GameObject.Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision OBJ)
+    {
+        if (OBJ.gameObject.tag == "Player")
+        {
+            AtRange = true;
+        }
+    }
+    private void OnCollisionStay(Collision OBJ)
+    {
+        if (OBJ.gameObject.tag == "Player")
+        {
+            AtRange = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision OBJ)
+    {
+        if (OBJ.gameObject.tag == "Player")
+        {
+            AtRange = false;
         }
     }
 }

f5569fcdf716cd5422fbf0b92d661b6be2f34635
Yonatan Golan <yongolan93@gmail.com>
2022-11-23 01:39:23 +0200

    Update Trader Dialogue

--- Assets/Scripts/NPC/Beaver/Trader.cs
+++ Assets/Scripts/NPC/Beaver/Trader.cs
@@ -7,8 +7,8 @@ public class Trader : MonoBehaviour
 {
     public GameObject Merchant;
     public Behavior Player;
-    public string Words;
-    public string PlayerResponse;
+    public GameObject TradeText;
+    public GameObject DialoguePanel;
     public Vector3 PlayerDistance;
     Quaternion FormalLook;
     // Start is called before the first frame update
@@ -24,12 +24,12 @@ public class Trader : MonoBehaviour
         if (PlayerDistance.magnitude<10)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(PlayerDistance), 0.1f);
-            Words = "Hey, you there! Buy Nuts with E and sell them with Q";
+            TradeText.SetActive(true);
+            DialoguePanel.SetActive(true);
             if (PlayerDistance.magnitude<6)
             {
                 if (Input.GetKeyDown(KeyCode.E) &&  Player.Currency>0)
                 {
-                    PlayerResponse = "Ok";
 
                     if (Player.Currency >=3)
                     {
@@ -39,18 +39,12 @@ public class Trader : MonoBehaviour
                 }
                 if (Input.GetKeyDown(KeyCode.Q) && Player.NutCount > 0)
                 {
-                    {
-                        PlayerResponse = "Ok";
-
                         if (Player.NutCount > 0)
                         {
                             Player.NutCount--;
                             Player.Currency += 3;
                         }
-                    }
                 }
-                else
-                    PlayerResponse = "";
 
 
 
@@ -58,7 +52,8 @@ public class Trader : MonoBehaviour
         }
         else
         {
-            Words = "Who's gonna grab my tiny nuts?";
+            TradeText.SetActive(false);
+            DialoguePanel.SetActive(false);
             transform.rotation = Quaternion.Slerp(transform.rotation, FormalLook, 0.1f);
         }
     }

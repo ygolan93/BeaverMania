c300876021f2b47e8303fe838f9efefa57c5a7a1
Yonatan Golan <yongolan93@gmail.com>
2022-11-28 16:51:43 +0200

    Update Lift Script

--- Assets/Scripts/Objects/Lift.cs
+++ Assets/Scripts/Objects/Lift.cs
@@ -7,6 +7,8 @@ public class Lift : MonoBehaviour
     [SerializeField] Rigidbody Beams;
     [SerializeField] float LiftSpeed;
     [SerializeField] float CurrentPos;
+    public int HighLimit = 18;
+    public int LowLimit = 18;
     bool Up;
     
 
@@ -31,11 +33,11 @@ public class Lift : MonoBehaviour
             Beams.velocity = new Vector3(0, -LiftSpeed, 0);
         }
 
-        if (CurrentPos>HeightStart+18)
+        if (CurrentPos>HeightStart+HighLimit)
         {
             Up = false;
         }
-        if (CurrentPos<HeightStart-18)
+        if (CurrentPos<HeightStart-LowLimit)
         {
             Up = true;
         }

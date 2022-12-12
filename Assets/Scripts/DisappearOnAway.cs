08a025ffcee9378258d6e55bcecc06b18925120c
Yonatan Golan <yongolan93@gmail.com>
2022-12-12 21:43:45 +0200

    Revert Disappear on distance script

--- Assets/Scripts/DisappearOnAway.cs
+++ Assets/Scripts/DisappearOnAway.cs
@@ -7,20 +7,34 @@ public class DisappearOnAway : MonoBehaviour
     [SerializeField] GameObject Item;
 
     public Behavior Player;
-
-    private void Start()
+    public Vector3 DistanceVec;
+    [SerializeField] float ActualDistance;
+    [SerializeField] float PopUpDistance;
+    private void Update()
     {
-        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Behavior>();
-    }
-    private void OnTriggerStay(Collider OBJ)
-    {
-        if (OBJ.gameObject == Player)
+        DistanceVec = Player.transform.position - Item.transform.position;
+        ActualDistance = Mathf.Abs(DistanceVec.magnitude);
+        if (ActualDistance < PopUpDistance)
+        {
             Item.SetActive(true);
-    }
-    private void OnTriggerExit(Collider OBJ)
-    {
-        if (OBJ.gameObject == Player)
+        }
+        else
             Item.SetActive(false);
     }
+
+    //private void Start()
+    //{
+    //    Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Behavior>();
+    //}
+    //private void OnTriggerStay(Collider OBJ)
+    //{
+    //    if (OBJ.gameObject == Player)
+    //        Item.SetActive(true);
+    //}
+    //private void OnTriggerExit(Collider OBJ)
+    //{
+    //    if (OBJ.gameObject == Player)
+    //        Item.SetActive(false);
+    //}
 
 }

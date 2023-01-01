7103985b9652a06434214e8a654dd1f9a582ae5f
Yonatan Golan <yongolan93@gmail.com>
2023-01-01 14:59:55 +0200

    Update destruction effects for trees, wasps and boss

--- Assets/Scripts/Objects/Newbridge/LogSpawner.cs
+++ Assets/Scripts/Objects/Newbridge/LogSpawner.cs
@@ -6,10 +6,10 @@ public class LogSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] Transform SpawnPoint;
-    [SerializeField] Transform Log1;
-    [SerializeField] Transform Log2;
-    [SerializeField] Transform Log3;
-    [SerializeField] Transform Log4;
+    public Transform[] Prefab;
+    //[SerializeField] Transform Log2;
+    //[SerializeField] Transform Log3;
+    //[SerializeField] Transform Log4;
 
 
     private void OnTriggerStay(Collider OBJ)
@@ -18,10 +18,10 @@ public class LogSpawner : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.Mouse0))
             {
-                Instantiate(Log1, SpawnPoint.position, Quaternion.identity);
-                Instantiate(Log2, SpawnPoint.position, Quaternion.identity);
-                Instantiate(Log3, SpawnPoint.position, Quaternion.identity);
-                Instantiate(Log4, SpawnPoint.position, Quaternion.identity);
+                foreach (var item in Prefab)
+                {
+                    Instantiate(item, SpawnPoint.position, Quaternion.identity);
+                }
                 Destroy(gameObject);
             }
         }

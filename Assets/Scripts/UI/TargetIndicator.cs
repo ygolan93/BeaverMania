069a53160e79c3da8a34b6381e885ee6fa7cc39d
Yonatan Golan <yongolan93@gmail.com>
2023-01-28 05:58:32 +0200

    WayPoint Indicators + BossArrow
    Add waypoint markers + general navigation arrow

---
+++ Assets/Scripts/UI/TargetIndicator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TargetIndicator : MonoBehaviour
+{
+    public Transform target;
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position-transform.position), rotationSpeed*Time.deltaTime);
+    }
+}

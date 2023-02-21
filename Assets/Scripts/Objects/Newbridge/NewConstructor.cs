6581e0d3acc4871cb37c9e744cb096706ea0a281
Yonatan Golan <yongolan93@gmail.com>
2023-02-21 05:14:03 +0200

    Fix Bridge mechanics
    Bridge now spawn one part at a time.
    Upon construction, for every 9 logs use 1 seed in order to spawn a connector unit

--- Assets/Scripts/Objects/Newbridge/NewConstructor.cs
+++ Assets/Scripts/Objects/Newbridge/NewConstructor.cs
@@ -7,6 +7,8 @@ public class NewConstructor : MonoBehaviour
     public int PartCount = 0;
     public int BridgeLimit = 8;
     [SerializeField] GameObject BridgePart;
+    [SerializeField] GameObject BridgeLink;
+    [SerializeField] GameObject Log;
     public AudioSource WoodKnock;
     public Rigidbody Bridge;
     public Material Cel;
@@ -28,6 +30,32 @@ public class NewConstructor : MonoBehaviour
         BridgeUI.text = BridgeText;
     }
 
+    public void OnCollisionEnter(Collision OBJ)
+    {
+        if (OBJ.gameObject.tag == "Part" && isLocked == true && PartCount < BridgeLimit)
+        {
+            PartCount++;
+            X = 4.45f;
+            var newPart = Instantiate(BridgePart, Bridge.transform.position + Bridge.transform.up * -X * PartCount + Bridge.transform.forward * -0.5f, Bridge.transform.rotation);
+            newPart.transform.parent = Bridge.transform;
+            Destroy(OBJ.gameObject);
+            WoodKnock.Play();
+            if (Player.CurrentHealth < Player.MaxHealth)
+            {
+                Player.CurrentHealth += 50;
+                Player.HealthBar.SetHealth(Player.CurrentHealth);
+            }
+
+        }
+
+        if (OBJ.gameObject.tag == "Seed" && isLocked == true && PartCount>=9)
+        {
+            Destroy(OBJ.gameObject);
+            BridgeLimit += 9;
+            var newPart = Instantiate(BridgeLink, Bridge.transform.position + Bridge.transform.up * -X * PartCount + Bridge.transform.forward * -0.5f, Bridge.transform.rotation);
+            newPart.transform.parent = Bridge.transform;
+        }
+    }
     public void OnTriggerStay(Collider OBJ)
     {
         if (OBJ.gameObject.tag == "Player")
@@ -40,32 +68,16 @@ public class NewConstructor : MonoBehaviour
                 BridgeText = "Locked";
                 BridgeUI.text = BridgeText;
             }
-            if (Input.GetKeyDown(KeyCode.Delete))
+            if (Input.GetKeyDown(KeyCode.Delete) && PartCount==0)
             {
+                for (int i = 0; i < 9; i++)
+                {
+                    Instantiate(Log, Bridge.transform.position+ new Vector3(0,i,0), Bridge.transform.rotation);
+                }
                 Destroy(gameObject);
             }
         }
-        if (OBJ.gameObject.tag == "Seed" && isLocked == true)
-        {
-            Destroy(OBJ.gameObject);
-            BridgeLimit += BridgeLimit;
-        }
 
-        if (OBJ.gameObject.tag == "Part" && isLocked == true && PartCount<BridgeLimit)
-        {
-            PartCount++;
-            WoodKnock.Play();
-            if (Player.CurrentHealth < Player.MaxHealth)
-            {
-                Player.CurrentHealth += 50;
-                Player.HealthBar.SetHealth(Player.CurrentHealth);
-            }
-            X = 4.45f;
-            var newPart = Instantiate(BridgePart, Bridge.transform.position + Bridge.transform.up * -X * PartCount + Bridge.transform.forward * -0.5f, Bridge.transform.rotation);
-            newPart.transform.parent = Bridge.transform;
-
-            Destroy(OBJ.gameObject);
-        }
     }
 }
 

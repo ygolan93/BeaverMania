cb173476cced87ca932e473848f404b318d3d624
Yonatan Golan <74872741+ygolan93@users.noreply.github.com>
2026-05-09 19:37:47 +0300

    Add safe player animator parameter hashes

---
+++ Assets/Scripts/Player/PlayerAnimatorParameters.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public static class PlayerAnimatorParameters
+{
+    public static readonly int Walk = Animator.StringToHash("walk");
+    public static readonly int Run = Animator.StringToHash("run");
+    public static readonly int Midair = Animator.StringToHash("midair");
+    public static readonly int Roll = Animator.StringToHash("roll");
+    public static readonly int Moving = Animator.StringToHash("moving");
+    public static readonly int Aim = Animator.StringToHash("aim");
+    public static readonly int Draw = Animator.StringToHash("draw");
+    public static readonly int StrafeForward = Animator.StringToHash("strafeForward");
+    public static readonly int StrafeBack = Animator.StringToHash("strafeBack");
+    public static readonly int StrafeLeft = Animator.StringToHash("strafeLeft");
+    public static readonly int StrafeRight = Animator.StringToHash("strafeRight");
+    public static readonly int Climb = Animator.StringToHash("climb");
+    public static readonly int Crouch = Animator.StringToHash("crouch");
+    public static readonly int Armor = Animator.StringToHash("armor");
+    public static readonly int Slash = Animator.StringToHash("slash");
+    public static readonly int Fight = Animator.StringToHash("fight");
+
+    public static bool TrySetBool(Animator animator, int hash, bool value)
+    {
+        if (animator == null)
+        {
+            return false;
+        }
+
+#if UNITY_EDITOR
+        if (!HasParameter(animator, hash, AnimatorControllerParameterType.Bool))
+        {
+            return false;
+        }
+#endif
+
+        animator.SetBool(hash, value);
+        return true;
+    }
+
+#if UNITY_EDITOR
+    static bool HasParameter(Animator animator, int hash, AnimatorControllerParameterType type)
+    {
+        AnimatorControllerParameter[] parameters = animator.parameters;
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            AnimatorControllerParameter parameter = parameters[i];
+            if (parameter.nameHash == hash && parameter.type == type)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+#endif
+}

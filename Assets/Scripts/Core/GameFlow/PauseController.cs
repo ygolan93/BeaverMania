0fbe7e2bf100a78bc586d17aa82796699246e1a6
Yonatan Golan <74872741+ygolan93@users.noreply.github.com>
2026-05-12 00:49:03 +0300

    Refine pause controller ownership

--- Assets/Scripts/Core/GameFlow/PauseController.cs
+++ Assets/Scripts/Core/GameFlow/PauseController.cs
@@ -2,77 +2,82 @@ using UnityEngine;
 
 namespace Beavermania.Core.GameFlow
 {
-public class PauseController : MonoBehaviour
-{
-    public bool ActivePause = false;
-
-    GameObject pauseMenu;
-    GameObject question;
-    global::Behaviour player;
-
-    public void Bind(GameObject pauseMenu, GameObject question, global::Behaviour player)
+    public class PauseController : MonoBehaviour
     {
-        this.pauseMenu = pauseMenu;
-        this.question = question;
-        this.player = player;
+        public bool ActivePause = false;
 
-        ActivePause = false;
-        ApplyPauseState();
-    }
+        GameObject pauseMenu;
+        GameObject question;
+        global::Behaviour player;
 
-    public void Pause()
-    {
-        ApplyPauseState();
-    }
-
-    public void HideQuestion()
-    {
-        SetQuestionVisible(false);
-    }
-
-    public void ChangeBolean()
-    {
-        ActivePause = !ActivePause;
-        ApplyPauseState();
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            ChangeBolean();
-    }
-
-    void ApplyPauseState()
-    {
-        if (ActivePause)
+        public void Bind(GameObject pauseMenu, GameObject question, global::Behaviour player)
         {
-            SetMenuVisible(true);
-            Time.timeScale = 0;
+            this.pauseMenu = pauseMenu;
+            this.question = question;
+            this.player = player;
 
-            if (player != null)
-                player.ShowCursor();
-
-            return;
+            ActivePause = false;
+            ApplyPauseState();
         }
 
-        SetMenuVisible(false);
-        Time.timeScale = 1f;
-        SetQuestionVisible(false);
+        public void Pause()
+        {
+            ApplyPauseState();
+        }
 
-        if (player != null && player.isAtTrader == false)
-            player.HideCursor();
-    }
+        public void HideQuestion()
+        {
+            SetQuestionVisible(false);
+        }
 
-    void SetMenuVisible(bool visible)
-    {
-        if (pauseMenu != null)
-            pauseMenu.SetActive(visible);
-    }
+        public void ChangeBolean()
+        {
+            SetPaused(!ActivePause);
+        }
 
-    void SetQuestionVisible(bool visible)
-    {
-        if (question != null)
-            question.SetActive(visible);
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                ChangeBolean();
+        }
+
+        void SetPaused(bool paused)
+        {
+            ActivePause = paused;
+            ApplyPauseState();
+        }
+
+        void ApplyPauseState()
+        {
+            if (ActivePause)
+            {
+                SetMenuVisible(true);
+                Time.timeScale = 0;
+
+                if (player != null)
+                    player.ShowCursor();
+
+                return;
+            }
+
+            SetMenuVisible(false);
+            Time.timeScale = 1f;
+            SetQuestionVisible(false);
+
+            if (player != null && player.isAtTrader == false)
+                player.HideCursor();
+        }
+
+        void SetMenuVisible(bool visible)
+        {
+            if (pauseMenu != null)
+                pauseMenu.SetActive(visible);
+        }
+
+        void SetQuestionVisible(bool visible)
+        {
+            if (question != null)
+                question.SetActive(visible);
+        }
     }
-}
 }
